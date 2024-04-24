7911971a9a9720da289256a785e6bf792108c7cb
Arya Swaroop <aryaswaroop027@gmail.com>
2024-04-24 21:41:24 +0530

    Add UI of TODO app

--- codeial/index.js
+++ codeial/index.js
@@ -10,10 +10,12 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 
+app.use(express.static('assets')); /* This line of code means its goes to your directory and find out the folder which name is "assets". */
+
 app.listen(port, function (err) {
     if (err) {
         console.log(`Error occure while running the server: ${err}`);
     }
 
-    console.log(`Successfully fire the server on port number: ${port}`);
+    console.log(`Successfully fire the server on: ${port}`);
 });
